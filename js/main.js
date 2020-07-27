287801023482153720a70856360765f1a9e7dad2
Alex <gurievalex78@gmail.com>
2020-07-28 00:21:14 +0200

    Guess

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/* Un alert espone 5 numeri casuali.
+Dopo la chiusura(manuale, cioè facendo click su ok) dell'alert, 
+parte un timer di 30 secondi.
+Dopo i 30 secondi l'utente deve inserire un prompt alla volta 
+i numeri che ha visto precedentemente. Dopo che sono stati inseriti i 5 
+numeri, il software dice quanti e
+quali dei numeri da indovinare sono stati individuati. */
+
+var list = [];
+while (list.length < 5) {
+    var numero = getRndInteger(1, 10);
+    if (!trovaElemento(list, numero)) {
+        list.push(numero);
+    }
+}
+alert(list);
+setTimeout(game, 3000); 
+
+
+function game() { 
+    var list1 = [];
+    var numeriUtente = [];
+    while (numeriUtente.length < 5) {
+        var numeri = parseInt(prompt('Inserisci i numeri visti precedentemente'));
+        if (trovaElemento(numeriUtente, numeri)) {
+            prompt('Numero già inserito!! Inserisci un numero diverso !');
+        }else {
+            numeriUtente.push(numeri);
+        }
+    }
+    var cont = 0;
+    for (var i = 0; i < list.length; i++) {
+        for (var j = 0; j < numeriUtente.length; j++) {
+            if (list[i] == numeriUtente[j]) {
+                list1.push(list[i]);
+                cont+=1;
+            }
+
+        }
+    }
+   
+    $('#numbers h2').text('Lista numeri generati:'+ ' ' + list);
+    if (list1.length > 0) {   
+        $('#numbers h3').text('Lista Numeri Trovati: ' + list1);
+        $('#numbers p').text('Hai azzeccato ' + cont + ' numeri su ' + list.length);
+    } else {
+        $('#numbers h3').text('Hai perso!');
+        $('#numbers p').text('Non hai azzeccato nessun numero');
+    }
+   /*  console.log(list);
+    console.log(numeriUtente);
+    console.log(list1); */
+ }
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function trovaElemento(lista, elemento) {
+    var i = 0;
+    while (i < lista.length) {
+        if (elemento == lista[i]) {
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
